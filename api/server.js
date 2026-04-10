ab85c85bea23f278091a0a8a5a94a533917d2424
Andrea Michelle Olivas Rivera <andreamichelleolivasrivera@gmail.com>
2026-04-10 17:22:29 -0600

    Update server.js

--- api/server.js
+++ api/server.js
@@ -4,22 +4,35 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Método no permitido' });
+    return res.status(405).json({ message: 'method not allowed' });
   }
 
   try {
     const { name, email, message } = req.body;
+    
+    console.log("Sending message..."); 
 
-    const data = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: 'Portfolio <onboarding@resend.dev>',
-      to: 'andreamichelleolivasrivera@gmail.com', 
-      html: `<p><strong>Nombre:</strong> ${name}</p>
-             <p><strong>Email:</strong> ${email}</p>
-             <p><strong>Mensaje:</strong> ${message}</p>`,
+      to: 'andreamichelleolivasrivera@gmail.com',
+      subject: `New message: ${name} `, 
+      html: `
+        <p><strong>name</strong> ${name}</p>
+        <p><strong>email:</strong> ${email}</p>
+        <p><strong>message:</strong> ${message}</p>
+      `,
     });
 
+    if (error) {
+      console.error("Error de Resend:", error);
+      return res.status(400).json(error);
+    }
+
+    console.log("Message sent successfully! ", data);
     return res.status(200).json({ success: true });
+
   } catch (error) {
+    console.error("Error in the server", error.message);
     return res.status(500).json({ error: error.message });
   }
 }
